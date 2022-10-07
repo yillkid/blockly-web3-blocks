fa25c3fa1b5efcbf34b813974e4924a4a492a17e
yillkid <yillkid@gmail.com>
2022-10-07 23:10:31 +0800

    feat: block

--- main.js
+++ main.js
@@ -10,19 +10,6 @@
 		console.log(e);
       });
   }
-  
-/*
-  function line_bot(data) {
-    $.post('https://script.google.com/macros/s/AKfycbz3WttppuI_dlw0vr7o6R_sJzY06kqBOjorHR1AI32sHj7gjkgP/exec',
-      data,
-      function(e){
-		console.log(e);
-      });
-  }
-*/
-
-  // window.line_ifttt = line_ifttt;
   window.line_notify = line_notify;
-  // window.line_bot = line_bot;
 
 }(window, window.document));
