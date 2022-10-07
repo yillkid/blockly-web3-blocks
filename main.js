50c341ce528fd992b1404bb126c413a885b39c6f
yillkid <yillkid@gmail.com>
2022-10-07 23:04:14 +0800

    feat: block

--- main.js
+++ main.js
@@ -1,24 +1,7 @@
 +(function (window, document) {
 
   'use strict';
-/*
-  function line_ifttt(event, key, v1, v2, v3) {
-    var obj = {};
-    if (v1 != "''" || v1.length > 0) {
-      obj.value1 = v1;
-    }
-    if (v2 != "''" || v1.length > 0) {
-      obj.value2 = v2;
-    }
-    if (v3 != "''" || v1.length > 0) {
-      obj.value3 = v3;
-    }
-    var uri = 'https://maker.ifttt.com/trigger/' + event + '/with/key/' + key;
-    $.post(uri, obj, function (e) {
-      console.log(e);
-    });
-  }
-*/
+  
   function line_notify(data) {
     console.log("hello QQQQ");
     $.post('https://script.google.com/macros/s/AKfycbx8CHul0yv0cPv-5DJLDz9hY1kZqQjm_7iLckjwKGEsjmtOuOc/exec',
@@ -28,6 +11,7 @@
       });
   }
   
+/*
   function line_bot(data) {
     $.post('https://script.google.com/macros/s/AKfycbz3WttppuI_dlw0vr7o6R_sJzY06kqBOjorHR1AI32sHj7gjkgP/exec',
       data,
@@ -35,9 +19,10 @@
 		console.log(e);
       });
   }
+*/
 
   // window.line_ifttt = line_ifttt;
   window.line_notify = line_notify;
-  window.line_bot = line_bot;
+  // window.line_bot = line_bot;
 
 }(window, window.document));
