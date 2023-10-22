0f61afdeb423af4d24835d8962d37d973815e00c
HuangJyunYu <yillkid@gmail.com>
2023-10-22 13:08:30 +0800

    Update blocks.js

--- new/blockly/blocks.js
+++ new/blockly/blocks.js
@@ -2,7 +2,7 @@ Blockly.Blocks['write_to_testnet'] = {
   init: function() {
     this.appendValueInput("value")
         .setCheck(null)
-        .appendField("寫入測試鏈...");
+        .appendField("寫入測試鏈");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(120);
